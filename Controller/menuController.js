b7698f26d89889655264d08da66cf0e8f333d861
buho1a <54486026+buho1a@users.noreply.github.com>
2023-05-17 17:52:28 -0500

    configuracion

---
+++ Controller/menuController.js
@@ -0,0 +1,12 @@
+function inicio(){
+    console.log("inicio");
+}
+function estadistica(){
+    console.log("estadistica");
+}
+function galeria(){
+    console.log("galeria");
+}
+function concurso(){
+    console.log("concurso");
+}
